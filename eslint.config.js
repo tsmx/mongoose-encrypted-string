f3731e1ce4a824f26959c1e048728ec5e6ff11ac
tsmx <dev@tsmx.net>
2024-08-08 20:46:27 +0200

    Moved to eslint 9.8, small code fixes

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+const { configs } = require('@eslint/js');
+const jest = require('eslint-plugin-jest');
+
+module.exports = [
+    configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'commonjs',
+            globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' }
+        },
+        rules: {
+            semi: 'error',
+            quotes: ['error', 'single'],
+            indent: ['error', 4, { 'SwitchCase': 1 }],
+            'no-unused-vars':
+                [
+                    'warn',
+                    {
+                        'varsIgnorePattern': '^_',
+                        'args': 'after-used',
+                        'argsIgnorePattern': '^_'
+                    }
+                ]
+        },
+        ignores: ['coverage/']
+    },
+    {
+        files: ['test/*.js'],
+        ...jest.configs['flat/recommended'],
+        rules: {
+            ...jest.configs['flat/recommended'].rules
+        }
+    }
+];
--- mongoose-encrypted-string.js
+++ mongoose-encrypted-string.js
@@ -1,10 +1,10 @@
-const SchemaType = require('mongoose').SchemaType
+const SchemaType = require('mongoose').SchemaType;
 const sc = require('@tsmx/string-crypto');
 
 class EncryptedString extends SchemaType {
     constructor(key, options) {
-        options.get = (v) => { return sc.decrypt(v, { key: EncryptedString.options.key, passNull: true }); }
-        options.set = (v) => { return sc.encrypt(v, { key: EncryptedString.options.key, passNull: true }); }
+        options.get = (v) => { return sc.decrypt(v, { key: EncryptedString.options.key, passNull: true }); };
+        options.set = (v) => { return sc.encrypt(v, { key: EncryptedString.options.key, passNull: true }); };
         super(key, options, 'EncryptedString');
     }
 
@@ -14,11 +14,11 @@ class EncryptedString extends SchemaType {
 
     static options = {
         key: null
-    }
+    };
 
 }
 
 module.exports.registerEncryptedString = function (mongoose, key) {
     EncryptedString.options.key = key;
     mongoose.Schema.Types.EncryptedString = EncryptedString;
-}
+};
